61c9fb3dce5b8a0a76fca766a32cf6401b18d1a5
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-10 00:38:45 +0200

    [UPD] button modalità budget

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  // stato che traccia la modalità
+  const [isBudgetModeActive, setIsBudgetModeActive] = useState(false);
+
+  // variabile che inverte la modalità
+  const toggleBudgetMode = () => {
+    setIsBudgetModeActive(!isBudgetModeActive);
+  };
   return (
     <nav>
       <ul className="list-unstyled d-flex">
@@ -12,6 +20,13 @@ const Navbar = () => {
         </li>
         <li className="me-3">
           <NavLink to="/products">Prodotti</NavLink>
+        </li>
+        <li>
+          <button onClick={toggleBudgetMode}>
+            {isBudgetModeActive
+              ? "Disattiva Modalità Budget"
+              : "Attiva Modalità Budget"}
+          </button>
         </li>
       </ul>
     </nav>

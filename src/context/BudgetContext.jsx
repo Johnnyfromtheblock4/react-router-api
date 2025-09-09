0ba0227c9ce51413d623fe1b1c1916d9f369a4ba
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-10 00:42:07 +0200

    [UPD] BudgetContext return

--- src/context/BudgetContext.jsx
+++ src/context/BudgetContext.jsx
@@ -6,7 +6,14 @@ const BudgetContext = createContext();
 // definisco il custom provider
 const BudgetProvider = ({ children }) => {
   // inserisco la variabile di stato
-  const { budgetMode, setBudgetMode } = useState(true);
+  const [budgetMode, setBudgetMode] = useState(true);
+
+  // restituisco il provider con il valore del contesto
+  return (
+    <BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+      {children}
+    </BudgetContext.Provider>
+  );
 };
 
 export default BudgetContext;

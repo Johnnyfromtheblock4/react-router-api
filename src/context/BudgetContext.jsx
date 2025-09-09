5028d7566877a4f9042000c002b22983834c9ac2
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-10 00:35:04 +0200

    [UPD] variabile di stato e provider

--- src/context/BudgetContext.jsx
+++ src/context/BudgetContext.jsx
@@ -1,5 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
+// creo il contesto
 const BudgetContext = createContext();
+
+// definisco il custom provider
+const BudgetProvider = ({ children }) => {
+  // inserisco la variabile di stato
+  const { budgetMode, setBudgetMode } = useState(true);
+};
 
 export default BudgetContext;
